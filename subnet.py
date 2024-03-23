1d55734a6dd76722dbb2be258650251554a69f72
Shamia Johnson <144083532+ShamiaJ@users.noreply.github.com>
2024-03-23 18:34:00 -0500

    Add files via upload

---
+++ subnet.py
@@ -0,0 +1,75 @@
+#enter your unique code
+uniquecode = 30701
+
+#problem 1: 155.227.130.227, CIDR: 26
+p1Network = '155.227.130.192'
+p1Broadcast = '155.227.130.255'
+p1First = '155.227.130.193'
+p1Last = '155.227.130.254'
+
+#problem 2: 66.212.92.13, CIDR: 29
+p2Network = '66.212.92.8'
+p2Broadcast = '66.212.92.15'
+p2First = '66.212.92.9'
+p2Last = '66.212.92.14'
+
+#problem 3: 233.197.54.55, CIDR: 27
+p3Network = '233.197.54.32'
+p3Broadcast = '233.197.54.63'
+p3First = '233.197.54.33'
+p3Last = '233.197.54.62'
+
+#problem 4: 144.182.16.98, CIDR: 24
+p4Network = '144.182.16.0'
+p4Broadcast = '144.182.16.255'
+p4First = '144.182.16.1'
+p4Last = '144.182.16.254'
+
+#problem 5: 55.167.233.140, CIDR: 28
+p5Network = '55.167.233.128'
+p5Broadcast = '55.167.233.143'
+p5First = '55.167.233.129'
+p5Last = '55.167.233.142'
+
+#problem 6: 222.152.195.183, CIDR: 25
+p6Network = '222.152.195.128'
+p6Broadcast = '222.152.195.255'
+p6First = '222.152.195.129'
+p6Last = '222.152.195.254'
+
+#problem 7: 133.138.157.225, CIDR: 29
+p7Network = '133.138.157.224'
+p7Broadcast = '133.138.157.231'
+p7First = '133.138.157.225'
+p7Last = '133.138.157.230'
+
+#problem 8: 44.123.119.12, CIDR: 26
+p8Network = '44.123.119.0'
+p8Broadcast = '44.123.119.63'
+p8First = '44.123.119.1'
+p8Last = '44.123.119.62'
+
+#problem 9: 211.108.80.54, CIDR: 24
+p9Network = '211.108.80.0'
+p9Broadcast = '211.108.80.255'
+p9First = '211.108.80.1'
+p9Last = '211.108.80.254'
+
+#problem 10: 122.93.42.97, CIDR: 27
+p10Network = '122.93.42.96'
+p10Broadcast = '122.93.42.127'
+p10First = '122.93.42.97'
+p10Last = '122.93.42.126'
+
+
+"""
+example:
+
+uniquecode = 12345
+
+problem 1
+p1Network = '199.204.14.128'
+p1Broadcast = '199.204.14.255'
+p1First = '199.204.14.129'
+p1Last =  '199.204.14.254'
+"""
